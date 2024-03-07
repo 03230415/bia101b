aef7e0a91149b9c0d486ba014b4d25f1c4613e06
03230415 <03230415.gcbs@rub.edu.bt>
2024-03-07 15:04:26 +0600

    string manipulations

---
+++ strings.py
@@ -0,0 +1,16 @@
+# string manipulation
+# string concatenation
+string1 = "hello "
+string2 = "world"
+combine = string1 + string2
+combined1 = "hi" + " " +"there"
+print ("string concatenation")
+print (combine)
+print (combined1)
+
+# string interpolation
+# "2"+"3" will give 23 because it is still str""
+name = "john"
+age = 30
+interpolated = f("john is (age) years of age")
+print(interpolated)
