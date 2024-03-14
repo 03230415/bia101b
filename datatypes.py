822d08e4900db3b9ec6b6736e37583608db259e3
03230415 <03230415.gcbs@rub.edu.bt>
2024-03-14 14:24:08 +0600

    datatype_sample

---
+++ datatypes.py
@@ -0,0 +1,40 @@
+# creation of array
+array1 = [1,2,3,'thimphu',2.5]
+print(array1)
+
+# # retrieving
+# access by index
+element1 = array1[0]
+print(element1)
+element2 = array1[3]
+print(element2)
+last_element = array1[-1]
+print(last_element)
+
+# slicing
+elements = array1[0:3]           #0 1 2, 3 is not included
+print(elements)
+
+arr1 = [1,2,3]
+arr2 = [chhukha, mongar]
+arr3 = arr1 + arr2
+print(arr3)
+
+# to check the number
+check_num = 2
+result = check_num in arr1
+print("result is",result)
+# '1' != 1
+
+
+# modifying the element
+array1[0] = 10
+print(array1[0])
+
+# getting numbers of elements in an array
+no_element = len(array1)
+print(no_element)
+
+# append
+arra=[1,2,3]
+arra.append(4)
