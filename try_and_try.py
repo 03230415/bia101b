36b9cc627e94cbfb84bf9606b90ec5afb015d8db
03230415 <03230415.gcbs@rub.edu.bt>
2024-03-22 11:55:41 +0600

    binary search

--- try_and_try.py
+++ try_and_try.py
@@ -9,3 +9,12 @@ length_s = len(s)
 print("counter",counter)
 print(len(s):,length_s)
 char = s[counter]   
+
+def greet():    #-this is function definition alone  and at the end we need to call the function as greet()
+                #all of the codes will be in the def function
+    # do some calculation code
+    # complex stuff
+    print("hello there")         
+greet()
+# calling function (function name and())
+# we need to call the function the way we defined the function
