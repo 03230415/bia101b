fb2e680258cca6d13be471aa4cce58fc8adad1f2
03230415 <03230415.gcbs@rub.edu.bt>
2024-04-26 11:51:57 +0600

    floor

--- my_duplicate.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        list = [1,2,3,1]
-        nums2 = set(int(nums))
-        for i in nums:
-            if nums != nums2:
-                return True
-            else:
-                return False
